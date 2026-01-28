31081a89c53f668c43bbcc198f147f55e9c768e9
Portuguez Padrón Angel <165244123+Fimbrostilk@users.noreply.github.com>
2026-01-28 04:49:10 -0600

    Refactor main function, styling and add system detection

--- Roadmap_Hard-Sofware.c
+++ Roadmap_Hard-Sofware.c
@@ -1,45 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int detectar_sistema() { // Para intentar saber en que sistema está corriendo xd
+    #ifdef _WIN32
+        return 1;
+    #elif __linux__
+        return 2;
+    #else
+        return 3;
+    #endif
+}
 
-    int opc_dis = 0;
+void limpiar_pantalla(void) { // Una función para limpiar la stdout
+        if (detectar_sistema() == 1) system("cls");
+        else system("clear");
+}
 
-    printf("Bienvenido a tu lista de Hardware.\n");
+void limpiar_buffer(void) { // Para que el getchar funcione bien
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+}
 
-    while (opc_dis != 1 && opc_dis != 2)
-    {
-        printf("\nElige el dispositivo.\n"
-               "\n1. Laptop\n"
-               "\n2. Celular\n\n"
-               "> ");
-        scanf("%i", &opc_dis);
 
-        if (opc_dis == 1)
-        {
-            printf("\nLaptop: HP ZBook 17 G1\n"
-                   "\nCPU: Intel Core i7-4800MQ (2.7GHz)\n"
-                   "GPU: Nvidia Quadro K4100M (4GB)\n"
-                   "RAM: 32 GB DDR3\n"
-                   "Hard Drives:\n\n"
-                   "    SSD SATA 477 GB\n"
-                   "        OS: Windows 11 Pro\n\n"
-                   "    HDD 500GB\n"
-                   "        OS: Linux Mint Cinnamon\n");
-        }
-        if (opc_dis == 2)
-        {
-            printf("\nCelular: Samsung Galaxy A25 5G\n"
-                   "CPU: Exynos 1280 octa-core (2.4GHz)\n"
-                   "GPU: Mali G68\n"
-                   "RAM: 8GB\n"
-                   "HD: 256 GB\n"
-                   "    OS: Android 16\n");
-        }
-    }
+/* --- Función principal --- */
 
-    printf("\n\nPresiona ENTER para cerrar la aplicacion.");
-    getchar();
-    getchar();
+int main(void) { // con void nomas para que sea robusto
 
+        char opc_dis; // Más cómodo trababar con caracteres
+
+        do {
+                limpiar_pantalla();
+                printf("Bienvenido a tu lista de Hardware.\n");
+                printf("\nElige el dispositivo.\n"
+                        "\t1. Laptop\n"
+                        "\t2. Celular\n"
+                        "\t3. Salir\n"
+                        "\t> ");
+                opc_dis = getchar(); if (opc_dis != '\n') limpiar_buffer(); // Capturamos y limpiamos el buffer para que el getchar de abajo funcione
+
+                if (opc_dis == '3') break; /* Para salir al instante */
+
+                /* En lugar de usar repetidas veces if, con switch-case es más limpio */
+                switch (opc_dis) {
+                        case '1':
+                                printf("\nLaptop: HP ZBook 17 G1\n"
+                                "\nCPU: Intel Core i7-4800MQ (2.7GHz)\n"
+                                "GPU: Nvidia Quadro K4100M (4GB)\n"
+                                "RAM: 32 GB DDR3\n"
+                                "Hard Drives:\n\n"
+                                "\tSSD SATA 477 GB\n"
+                                "\t\tOS: Windows 11 Pro\n\n"
+                                "\tHDD 500GB\n"
+                                "\t\tOS: Linux Mint Cinnamon\n");
+                                break;
+                        case '2':
+                                printf("\nCelular: Samsung Galaxy A25 5G\n" // Yo tengo un A26 xd
+                                "CPU: Exynos 1280 octa-core (2.4GHz)\n"
+                                "GPU: Mali G68\n"
+                                "RAM: 8GB\n"
+                                "HD: 256 GB\n"
+                                "\tOS: Android 16\n");
+                                break;
+                }
+
+                printf("\n\nIntro para continuar, '3' para salir.\n");
+                opc_dis = getchar(); if (opc_dis != '\n') limpiar_buffer(); // Lo mismo del getchar() de arriba
+
+        } while (opc_dis != '3'); // Nomas porque me gusta usar do-while */
+
+        return 0; // Indica que el programa terminó correctamente
+}
+// PD: es buena práctica documentar tus programas.
     return 0;
 }
